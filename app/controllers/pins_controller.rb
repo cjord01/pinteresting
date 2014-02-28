befdd0a86979f51fe40123bae1fb341511d27101
Clayton Jordan <clayjchicago@gmail.com>
2014-02-28 15:13:02 -0600

    Hoping this solves the Migrate problem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  def index
+   def index
     @pins = Pin.all
   end
 
@@ -52,6 +52,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
